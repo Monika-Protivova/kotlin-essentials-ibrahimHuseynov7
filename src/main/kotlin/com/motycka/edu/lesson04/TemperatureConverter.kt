510e50d3696b13945b1cac77376c97c3e39d6cb5
ibrahimHuseynov7 <118387768+ibrahimHuseynov7@users.noreply.github.com>
2025-06-16 14:43:48 +0400

    Update TemperatureConverter.kt

--- src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
+++ src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
@@ -1,201 +1,44 @@
-package com.motycka.edu.lesson04
+package com.motycka.edu.lesson04Add commentMore actions
 
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.doubles.shouldBeExactly
-import io.kotest.matchers.doubles.shouldBeWithinPercentageOf
-import io.kotest.matchers.shouldBe
+import io.github.oshai.kotlinlogging.KotlinLogging
 
-class TemperatureConverterTest : StringSpec({
+private val logger = KotlinLogging.logger { }
 
-    // Existing tests for toFahrenheit method
-    "convert Celsius to Fahrenheit where celsius is 0" {
-        // given
-        val celsius = 0.0
-        
-        // when
-        val fahrenheit = TemperatureConverter.toFahrenheit(celsius)
-        
-        // then
-        fahrenheit shouldBe 32.0
+/**
+ * Utility object for converting temperature values between Fahrenheit and Celsius
+ */
+object TemperatureConverter {
+
+    /**
+     * Converts temperature value given in Fahrenheit to Celsius
+     *
+     * @param fahrenheit temperature value in Fahrenheit
+     * @return temperature value in Celsius
+     * @see <a href="https://en.wikipedia.org/wiki/Fahrenheit">Fahrenheit</a>
+     * @see <a href="https://en.wikipedia.org/wiki/Celsius">Celsius</a>
+     */
+    fun toCelsius(fahrenheit: Double): Double {
+        logger.info { "Converting $fahrenheit Fahrenheit to Celsius" }
+        return (fahrenheit - 32) * 5 / 9
     }
 
-    "convert Celsius to Fahrenheit where celsius is 100" {
-        // given
-        val celsius = 100.0
-        
-        // when
-        val fahrenheit = TemperatureConverter.toFahrenheit(celsius)
-        
-        // then
-        fahrenheit shouldBe 212.0
+    /**
+     * Converts temperature value given in Celsius to Fahrenheit
+     *
+     * @param celsius temperature value in Celsius
+     * @return temperature value in Fahrenheit
+     * @see <a href="https://en.wikipedia.org/wiki/Fahrenheit">Fahrenheit</a>
+     * @see <a href="https://en.wikipedia.org/wiki/Celsius">Celsius</a>
+     */
+    fun toFahrenheit(celsius: Double): Double {
+        logger.info { "Converting $celsius Celsius to Fahrenheit" }
+        return celsius * 9 / 5 + 32
     }
 
-    "convert Celsius to Fahrenheit where celsius is 37" {
-        // given
-        val celsius = 37.0
-        
-        // when
-        val fahrenheit = TemperatureConverter.toFahrenheit(celsius)
-        
-        // then
-        fahrenheit shouldBe 98.6
-    }
+}
 
-    // New tests for toCelsius method
-    "convert Fahrenheit to Celsius where fahrenheit is 32" {
-        // given
-        val fahrenheit = 32.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBe 0.0
-    }
 
-    "convert Fahrenheit to Celsius where fahrenheit is 212" {
-        // given
-        val fahrenheit = 212.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBe 100.0
-    }
-
-    "convert Fahrenheit to Celsius where fahrenheit is 98.6" {
-        // given
-        val fahrenheit = 98.6
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBeWithinPercentageOf(37.0, 0.01)
-    }
-
-    "convert negative Fahrenheit to Celsius where fahrenheit is -40" {
-        // given
-        val fahrenheit = -40.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBe -40.0
-    }
-
-    "convert negative Fahrenheit to Celsius where fahrenheit is -4" {
-        // given
-        val fahrenheit = -4.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBe -20.0
-    }
-
-    // Bonus challenge tests - Edge cases
-    "convert very large Fahrenheit value to Celsius" {
-        // given
-        val fahrenheit = 1000.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBeWithinPercentageOf(537.78, 0.01)
-    }
-
-    "convert very small Fahrenheit value to Celsius" {
-        // given
-        val fahrenheit = -459.67 // absolute zero in Fahrenheit
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBeWithinPercentageOf(-273.15, 0.01)
-    }
-
-    "test round-trip conversion from Celsius to Fahrenheit and back" {
-        // given
-        val originalCelsius = 25.0
-        
-        // when
-        val fahrenheit = TemperatureConverter.toFahrenheit(originalCelsius)
-        val backToCelsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        backToCelsius shouldBeExactly originalCelsius
-    }
-
-    "test round-trip conversion from Fahrenheit to Celsius and back" {
-        // given
-        val originalFahrenheit = 77.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(originalFahrenheit)
-        val backToFahrenheit = TemperatureConverter.toFahrenheit(celsius)
-        
-        // then
-        backToFahrenheit shouldBeWithinPercentageOf(originalFahrenheit, 0.001)
-    }
-
-    "convert room temperature Fahrenheit to Celsius" {
-        // given
-        val fahrenheit = 68.0 // typical room temperature in Fahrenheit
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(fahrenheit)
-        
-        // then
-        celsius shouldBe 20.0
-    }
-
-    "convert freezing point of water from Celsius to Fahrenheit" {
-        // given
-        val freezingCelsius = 0.0
-        
-        // when
-        val fahrenheit = TemperatureConverter.toFahrenheit(freezingCelsius)
-        
-        // then
-        fahrenheit shouldBe 32.0
-    }
-
-    "convert freezing point of water from Fahrenheit to Celsius" {
-        // given
-        val freezingFahrenheit = 32.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(freezingFahrenheit)
-        
-        // then
-        celsius shouldBe 0.0
-    }
-
-    "convert boiling point of water from Celsius to Fahrenheit" {
-        // given
-        val boilingCelsius = 100.0
-        
-        // when
-        val fahrenheit = TemperatureConverter.toFahrenheit(boilingCelsius)
-        
-        // then
-        fahrenheit shouldBe 212.0
-    }
-
-    "convert boiling point of water from Fahrenheit to Celsius" {
-        // given
-        val boilingFahrenheit = 212.0
-        
-        // when
-        val celsius = TemperatureConverter.toCelsius(boilingFahrenheit)
-        
-        // then
-        celsius shouldBe 100.0
-    }
-})
+fun main() {
+    TemperatureConverter.toFahrenheit(34.0).also(::println)
+    TemperatureConverter.toCelsius(94.0).also(::println)
+}
